880a7da273005a9fd4169f44ec0b578e8624f626
CpedaMonsterEnergyLover <v.yagnaev@team.globus-ltd.com>
2025-02-11 12:44:59 +0300

    get chat id via command

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, CallbackContext
+from telegram.ext import Application, CommandHandler, filters, CallbackContext
 from decouple import config
 
 BOT_TOKEN = config('BOT_TOKEN')
@@ -8,9 +8,9 @@ app = Application.builder().token(BOT_TOKEN).build()
 
 async def reply_with_chat_id(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
-    await update.message.reply_text(str(chat_id), parse_mode="Markdown")
+    await update.message.reply_text(f"Айди чата: {str(chat_id)}", parse_mode="Markdown")
 
-app.add_handler(MessageHandler(filters.ALL, reply_with_chat_id))
+app.add_handler(CommandHandler("start", reply_with_chat_id))
 
 if __name__ == "__main__":
     app.run_polling()
